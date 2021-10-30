0a8b8025afe19c7afe572c14e25b19a55bedad02
jerwinwieser <j2.wieser@gmail.com>
2021-10-31 00:08:59 +0200

    add update form

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('list/', views.VacancyListView.as_view(), name='vacancy_list'),
     path('create/', views.VacancyCreateView.as_view(), name='vacancy_create'),
+    path('detail/<int:pk>', views.VacancyDetailView.as_view(), name='vacancy_detail'),
     path('update/<int:pk>/', views.VacancyUpdateView.as_view(), name='vacancy_update'),
     path('delete/<int:pk>/', views.VacancyDeleteView.as_view(), name='vacancy_delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from core.models import Vacancy
 
 class IndexView(TemplateView):
@@ -12,8 +13,12 @@ class VacancyListView(ListView):
 
 class VacancyCreateView(CreateView):
     model = Vacancy
+    template_name = 'core/vacancy_create.html'
     fields = '__all__'
     success_url = reverse_lazy('vacancy_list')
+
+class VacancyDetailView(DetailView):
+    model = Vacancy
 
 class VacancyUpdateView(UpdateView):
     model = Vacancy
